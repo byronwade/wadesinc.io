c39b64aa7ad3674f5c53f7f03c335d1528204410
Byron <bcw1995@gmail.com>
2022-07-15 20:54:35 -0700

    new

---
+++ template-parts/contact-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Contact Page
+ * 
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package wadesinc
+ */
+
+?>
+
+<div>
+	<?php get_header() ?>
+
+	<div class="page-content">
+		<?php echo apply_shortcodes( '[contact-form-7 id="34" title="Contact Form"]' ); ?>
+	</div><!-- .page-content -->
+</div>
---
+++ template-parts/promise-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Promise Page
+ * 
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package wadesinc
+ */
+
+?>
+
+<div>
+	<?php get_header() ?>
+
+	<div class="page-content">
+		hi
+	</div><!-- .page-content -->
+</div>
