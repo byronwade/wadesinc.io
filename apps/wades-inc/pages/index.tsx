690df7a348355bc730a1ed8d611705e571a418df
Byron Wade <bcw1995@gmail.com>
2023-03-08 21:29:41 -0800

    turbo for the win

---
+++ apps/wades-inc/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function WadesInc() {
+	return (
+		<div>
+			<h1>Wades Inc</h1>
+			<Button />
+		</div>
+	);
+}
---
+++ apps/wades-plumbing-and-septic/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function WadesPlumbingandSeptic() {
+	return (
+		<div>
+			<h1>Wades Plumbing and Septic</h1>
+			<Button />
+		</div>
+	);
+}
