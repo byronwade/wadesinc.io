86f1cdaf28084d606281e99b3d8f6e258e5769c1
Byron Wade <bcw1995@gmail.com>
2023-03-15 11:57:01 -0700

    seo

--- apps/clogmonsterssepticpumping.com/src/app/page.tsx
+++ apps/clogmonsterssepticpumping.com/src/app/page.tsx
@@ -1,3 +1,29 @@
+export const metadata = {
+	title: {
+		default: "Clog Monsters Septic Pumping",
+		template: "%s | Clog Monsters Septic Pumping",
+	},
+	description: "Clog Monsters Septic Pumping is a...",
+	openGraph: {
+		title: "Clog Monsters Septic Pumping",
+		description: "The React Framework for the Web",
+		type: "article",
+		publishedTime: "2023-01-01T00:00:00.000Z",
+		authors: ["Seb", "Josh"],
+		url: "https://nextjs.org",
+		images: [
+			{
+				url: "https://nextjs.org/og.png",
+				width: 800,
+				height: 600,
+			},
+		],
+	},
+	robots: {
+		index: true,
+	},
+};
+
 export default function Home() {
 	return (
 		<main>
--- apps/wadesplumbingandseptic.com/src/app/page.tsx
+++ apps/wadesplumbingandseptic.com/src/app/page.tsx
@@ -1,3 +1,29 @@
+export const metadata = {
+	title: {
+		default: "Wades Plumbing and Septic",
+		template: "%s | Wades Plumbing and Septic",
+	},
+	description: "Wades Plumbing and Septic is a...",
+	openGraph: {
+		title: "Wades Plumbing and Septic",
+		description: "The React Framework for the Web",
+		type: "article",
+		publishedTime: "2023-01-01T00:00:00.000Z",
+		authors: ["Seb", "Josh"],
+		url: "https://nextjs.org",
+		images: [
+			{
+				url: "https://nextjs.org/og.png",
+				width: 800,
+				height: 600,
+			},
+		],
+	},
+	robots: {
+		index: true,
+	},
+};
+
 export default function Home() {
 	return (
 		<main>
