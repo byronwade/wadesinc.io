a8b16474f98047851e236c47f0f4a8fb12e53c5f
Byron Wade <bcw1995@gmail.com>
2023-03-27 00:23:45 -0700

    server side

--- apps/wadesplumbingandseptic.com/src/app/layout.tsx
+++ apps/wadesplumbingandseptic.com/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/sections/Header";
 import Footer from "@/components/sections/Footer";
 import "./globals.css";
+import Script from "next/script";
 // import Script from "next/script";
 
 async function HeaderLinks() {
@@ -135,6 +136,16 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 				<Header data={data} />
 				{children}
 				<Footer />
+				<Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-6TLN795BRR"></Script>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+
+						gtag('config', 'G-6TLN795BRR');
+					`}
+				</Script>
 				{/* <Script id="ze-snippet" strategy="lazyOnload" src="https://static.zdassets.com/ekr/snippet.js?key=06e45130-bfd2-4b2b-8137-28903b96f527"></Script> */}
 			</body>
 		</html>
