b45d99e5247bb42dcea5ad1b67e6eddddc2734bb
Byron Wade <bcw1995@gmail.com>
2023-03-28 14:21:35 -0700

    test

--- apps/wadesplumbingandseptic.com/src/app/expert-tips/page.tsx
+++ apps/wadesplumbingandseptic.com/src/app/expert-tips/page.tsx
@@ -4,7 +4,6 @@ import Pagnation from "@/components/ui/Pagnation";
 import Search from "@/components/ui/Search";
 import { ArrowLongRightIcon } from "@heroicons/react/20/solid";
 import { Suspense } from "react";
-import Loading from "./loading";
 
 async function getTips() {
 	const { data } = await fetch("https://wadesplumbingandseptic.byronw35.sg-host.com/graphql", {
@@ -125,7 +124,7 @@ export default async function ExpertTips() {
 						{/* <Search placeholder="Search for a tip..." /> */}
 
 						<div className="grid gird-col-1 md:grid-cols-2 lg:grid-col-3 xl:grid-cols-4 auto-rows-auto gap-6 mt-10 items-stretch w-full">
-							<Suspense fallback={<p>Loading content...</p>}>
+							<>
 								{tips.map((tip, index) => {
 									if (index % 7 === 0) {
 										return (
@@ -163,7 +162,7 @@ export default async function ExpertTips() {
 										);
 									}
 								})}
-							</Suspense>
+							</>
 						</div>
 
 						{/* <Pagnation pageMin={1} pageMax={1000} total={2000} /> */}
