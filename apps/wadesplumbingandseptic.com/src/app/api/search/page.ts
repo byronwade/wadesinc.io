aab113d617f4dc0539af2ecf1a0b4e635d0b7d48
Byron Wade <bcw1995@gmail.com>
2023-06-24 12:12:50 -0700

    more

--- apps/wadesplumbingandseptic.com/src/app/api/search/page.ts
+++ apps/wadesplumbingandseptic.com/src/app/api/search/page.ts
@@ -9,9 +9,13 @@ export default async function handler(request: Request) {
 	try {
 		const { searchParams } = new URL(request.url);
 		const searchQuery = searchParams.get("q");
-		const { data } = await getService(searchQuery);
+		const response = await getService(searchQuery);
 
-		return NextResponse.json({ data });
+		if (!response) {
+			return new Response("Service returned null", { status: 500 });
+		}
+
+		return NextResponse.json({ data: response.data });
 	} catch (e: any) {
 		console.log(`${e.message}`);
 		return new Response(`Failed to generate the image`, {
